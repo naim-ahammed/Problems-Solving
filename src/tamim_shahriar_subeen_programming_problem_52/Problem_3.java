b51b66e15d31d6b7f1aaa873c5cf4a49f49e762b
naim-ahammed <125636794+naim-ahammed@users.noreply.github.com>
2023-11-27 11:41:12 +0600

    problem of tamim shahriar subeen. problem output show Each line will have a total of five numbers And each number must be separated by '\t'(tab) characters

---
+++ src/tamim_shahriar_subeen_programming_problem_52/Problem_3.java
@@ -0,0 +1,20 @@
+package tamim_shahriar_subeen_programming_problem_52;
+
+public class Problem_3 {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+
+        for (int i = 1000; i >= 1; i--) {
+            System.out.print(i + "\t");
+            count++;
+
+            // Check if five numbers have been printed
+            if (count == 5) {
+                System.out.println(); // Move to the next line
+                count = 0; // Reset the count
+            }
+        }
+    }
+}

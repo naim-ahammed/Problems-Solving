3281379a7b57bc3e5ef7730558c15415acda4591
naim-ahammed <125636794+naim-ahammed@users.noreply.github.com>
2024-12-02 22:21:58 +0600

    Problem 2769. Find the Maximum Number of Non-Overlapping Subarrays of Given Length

---
+++ src/LeetCode/Problem_2769.java
@@ -0,0 +1,16 @@
+package LeetCode;
+
+public class Problem_2769 {
+
+    // Problem 2769. Find the Maximum Number of Non-Overlapping Subarrays of Given Length
+
+    public static void main(String[] args) {
+
+        int num = 4;
+        int t = 1;
+
+        int sum = num + 2 * t;
+
+        System.out.println(sum);
+    }
+}
